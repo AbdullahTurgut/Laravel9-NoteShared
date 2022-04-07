4a95d7800ed07f0198a38e998260d289b328eeeb
AbdullahTurgut <abdullah.trgt4196@gmail.com>
2022-04-08 02:38:27 +0300

    AdminPanel Category Index Page's Datas Table Retrieving From Database Created

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -19,17 +19,42 @@
                                 <p>Category List</p>
                             </div>
                         </div>
-                        <div class="d-flex justify-content-between align-items-end flex-wrap">
-                            <button type="button" class="btn btn-light bg-white btn-icon me-3 d-none d-md-block ">
-                                <i class="mdi mdi-download text-muted"></i>
-                            </button>
-                            <button type="button" class="btn btn-light bg-white btn-icon me-3 mt-2 mt-xl-0">
-                                <i class="mdi mdi-clock-outline text-muted"></i>
-                            </button>
-                            <button type="button" class="btn btn-light bg-white btn-icon me-3 mt-2 mt-xl-0">
-                                <i class="mdi mdi-plus text-muted"></i>
-                            </button>
-                            <button class="btn btn-primary mt-2 mt-xl-0">Generate report</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-12 grid-margin stretch-card">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title">Category List</h4>
+                        <div class="table-responsive pt-3">
+                            <table class="table table-bordered">
+                                <thead>
+                                <tr>
+                                    <th>Id</th>
+                                    <th>Title</th>
+                                    <th>Keywords</th>
+                                    <th>Description</th>
+                                    <th>Image</th>
+                                    <th>Status</th>
+                                    <th>Edit</th>
+                                    <th>Delete</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($data as $rs)
+                                <tr>
+                                    <td>{{$rs->id}}</td>
+                                    <td>{{$rs->title}}</td>
+                                    <td>{{$rs->keywords}}</td>
+                                    <td>{{$rs->description}}</td>
+                                    <td>{{$rs->image}}</td>
+                                    <td>{{$rs->status}}</td>
+                                    <td><a href="/admin/category/edit{{$rs->id}}/">Edit</a></td>
+                                    <td><a href="/admin/category/delete{{$rs->id}}/">Delete</a></td>
+                                </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
