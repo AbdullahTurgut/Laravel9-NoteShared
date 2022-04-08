26ee2c438768985980658e82a3e9c1d03ffd5f78
AbdullahTurgut <abdullah.trgt4196@gmail.com>
2022-04-09 02:19:48 +0300

    Update Function Created

--- resources/views/admin/category/edit.blade.php
+++ resources/views/admin/category/edit.blade.php
@@ -12,7 +12,7 @@
                     <div class="d-flex justify-content-between flex-wrap">
                         <div class="d-flex align-items-end flex-wrap">
                             <div class="me-md-3 me-xl-5">
-                                <h2>Edit Category</h2>
+                                <h2>Edit {{$data->title}}</h2>
                             </div>
                             <div class="d-flex">
                                 <p class="text-muted mb-0 hover-cursor"><a href="/admin">Home</a>&nbsp;/&nbsp;</p>
@@ -23,19 +23,19 @@
                 </div>
             </div>
             <h4 class="card-title">Category Elements</h4>
-            <form class="forms-sample" action="/admin/category/store" method="post">
+            <form class="forms-sample" action="/admin/category/update/{{$data->id}}" method="post">
                 @csrf
                 <div class="form-group">
                     <label for="exampleInputEmail3">Title</label>
-                    <input type="text" class="form-control" name="title" placeholder="Title">
+                    <input type="text" class="form-control" name="title" value="{{$data->title}}">
                 </div>
                 <div class="form-group">
                     <label for="exampleInputEmail3">Keywords</label>
-                    <input type="text" class="form-control" name="keywords" placeholder="Keywords">
+                    <input type="text" class="form-control" name="keywords" value="{{$data->keywords}}">
                 </div>
                 <div class="form-group">
                     <label for="exampleInputEmail3">Description</label>
-                    <input type="text" class="form-control" name="description" placeholder="Description">
+                    <input type="text" class="form-control" name="description" value="{{$data->description}}">
                 </div>
                 <div class="form-group">
                     <label>Image</label>
@@ -50,12 +50,12 @@
                 <div class="form-group">
                     <label for="exampleFormControlSelect2">Status</label>
                     <select class="form-control" name="status">
+                        <option selected>{{$data->status}}</option>
                         <option>True</option>
                         <option>False</option>
                     </select>
                 </div>
-                <button type="submit" class="btn btn-primary me-2">Save</button>
-                <button class="btn btn-light">Cancel</button>
+                <button type="submit" class="btn btn-primary me-2">Update</button>
             </form>
         </div>
         <!-- content-wrapper ends -->
