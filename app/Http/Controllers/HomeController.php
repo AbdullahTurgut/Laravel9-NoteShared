ff5061fb54ff7e06c860a28b1f6ae43a98449eae
AbdullahTurgut <abdullah.trgt4196@gmail.com>
2022-04-24 10:31:46 +0300

    Added Images To Home Slider From Storage

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Content;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     //
+
     public function index(){
-        return view('home.index');
+        $sliderdata = Content::limit(5)->get();
+        return view('home.index',[
+            'sliderdata'=>$sliderdata
+        ]);
     }
 }
